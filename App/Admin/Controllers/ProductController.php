9f3621556628856816d4195cb042b21574f6d9ad
tunv <nguyenvantu191298@gmail.com>
2020-10-01 11:18:51 +0700

    open modal image

--- App/Admin/Controllers/ProductController.php
+++ App/Admin/Controllers/ProductController.php
@@ -2,16 +2,22 @@
 
 namespace App\Admin\Controllers;
 
+use App\Config;
 use Core\View;
 
 class ProductController
 {
     public function index()
     {
+        View::renderTemplate('Admin', 'pages/all-product.html', ['title' => 'All Products']);
     }
     public function allProduct()
     {
         View::renderTemplate('Admin', 'pages/all-product.html', ['title' => 'All Products']);
     }
-    
+
+    public function addNewProductView()
+    {
+        View::renderTemplate('admin', 'pages/upload-product.html', ['title' => 'Add new Product', 'base_url' => Config::BASE_URL]);
+    }
 }
