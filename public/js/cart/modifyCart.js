59fc5550a7fd0d2e63d6d1d9410b342dd981440c
tunv <nguyenvantu191298@gmail.com>
2020-10-04 23:22:59 +0700

    checkout

--- public/js/cart/modifyCart.js
+++ public/js/cart/modifyCart.js
@@ -1,7 +1,14 @@
 var rmBtn = document.querySelectorAll(".rmItem");
 
+console.log(rmBtn);
+
 rmBtn.forEach(function (btn) {
     btn.addEventListener("click", function () {
         console.log(btn.getAttribute("data-sku"))
     })
 });
+
+function removeAllItem() {
+    localStorage.removeItem("cart");
+    alert("removed all items");
+}
