a51f15c0028cf01e351e9eea3912552d138b2345
tunv <nguyenvantu191298@gmail.com>
2020-10-06 12:44:35 +0700

    add route for user

--- App/Admin/Controllers/UserController.php
+++ App/Admin/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Core\View;
 class UserController{
     public function index()
     {
-        View::renderTemplate('admin', 'pages/list-user.html', [
+        View::renderTemplate('admin', 'pages/all-user.html', [
             'base_url' => Config::BASE_URL,
             'title' => 'All User'
         ]);
